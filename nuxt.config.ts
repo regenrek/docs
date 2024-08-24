e80044c4e2cd0e3ea29e0fe42bb6e74161e4f4be
regenrek <hello@regenrek.at>
2024-08-24 04:40:54 +0200

    update v3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,56 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
+  extends: ["@nuxt/ui-pro"],
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    'nuxt-og-image'
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxthq/studio",
+    "nuxt-og-image",
   ],
 
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton', 'UIcon'].includes(c.pascalName))
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        ["UButton", "UIcon"].includes(c.pascalName)
+      );
 
-      globals.forEach(c => c.global = true)
-    }
+      globals.forEach((c) => (c.global = true));
+    },
   },
 
   colorMode: {
-    disableTransition: true
+    disableTransition: true,
   },
 
   routeRules: {
     // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
-    '/': { prerender: true },
-    '/api/search.json': { prerender: true }
+    "/": { prerender: true },
+    "/api/search.json": { prerender: true },
   },
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
   typescript: {
-    strict: false
+    strict: false,
   },
-
-  future: {
-    compatibilityVersion: 4
-  },
-
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
-  compatibilityDate: '2024-07-11'
-})
+  compatibilityDate: "2024-07-11",
+});
